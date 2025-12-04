c69873bd20e93d06f8e45ee21823fd55d59aff70
Victor <ve.castro21@info.uas.edu.mx>
2025-12-04 00:53:07 -0700

    patch: tsconfig.json fixed + structure

---
+++ src/vite.env.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string;
+  readonly VITE_APP_NAME: string;
+  // Agregar más variables de entorno aquí si es necesario
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
